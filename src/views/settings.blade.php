ff9f140633c96c7d98c83f4f050b9180c4495e20
terakyan <edo.terakyan@gmail.com>
2017-12-05 06:53:56 -0800

    clean

--- src/views/settings.blade.php
+++ src/views/settings.blade.php
@@ -48,73 +48,6 @@
         {{--all and singel settings--}}
         <div class="col-md-6">
             <div class="panel panelSettingData">
-                <div class="panel-heading" role="tab" id="headingTwo">
-                    <h4 class="panel-title"><a role="button" data-toggle="collapse" data-parent="#accordion"
-                                               href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"> <i
-                                    class="glyphicon glyphicon-chevron-right"></i>All settings</a></h4>
-                </div>
-                <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
-                    <div class="panel-body">
-                        {!! Form::model($all) !!}
-                        {!! Form::hidden('id',null) !!}
-                        <div class="form-group">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    {{Form::hidden('header',0)}}
-                                    {{Form::hidden('footer',0)}}
-                                    <label class="bd_layout pull-left m-r-15">{!! Form::checkbox('header',1,null,['style' => 'position:initial;z-index:1;']) !!}
-                                        <span class="labls">Header</span>
-
-                                    </label>
-                                    <label class="bd_layout"> {!! Form::checkbox('footer',1,null,['style' => 'position:initial;z-index:1;']) !!}
-                                        <span class="labls">Footer</span>
-
-                                    </label>
-                                </div>
-                                <div class="col-md-12 m-b-10">
-                                    <div class="col-sm-4 p-l-0">Page Layout</div>
-                                    <div class="col-sm-5 p-l-0 p-r-10">
-                                        <input data-key="title" readonly="readonly"
-                                               class="page-layout-title form-control"
-                                               value="@if($all->page_layout){!! BBgetLayoutAttr($all->page_layout,"title") !!}@else{!! 'Nothing selected' !!} @endif">
-                                    </div>
-                                    {!! BBbutton2('layouts','page_layout','front_pages_layout',($all->page_layout)?'Change':'Select',['class'=>'btn btn-default change-layout','model' =>$all->page_layout]) !!}
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="row">
-                                <div class="col-md-12 m-b-10">
-                                    <div class="col-sm-4 p-l-0">Main Unit</div>
-                                    <div class="col-sm-5 p-l-0 p-r-10">
-                                        <input data-key="title" readonly="readonly"
-                                               class="page-layout-title form-control"
-                                               value="@if($all->main_content){!! BBgetUnitAttr($all->main_content,"title") !!}@else{!! 'Nothing selected' !!} @endif">
-                                    </div>
-                                    {!! BBbutton2('unit','main_content','all_posts',($all->main_content)?'Change':'Select',['class'=>'btn btn-default change-layout','model' =>$all->main_content]) !!}
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="row">
-                                <label for="navstyle" class="col-xs-3">Page Title</label>
-                                <div class="col-xs-9">
-                                    {!! Form::text('title',null,['class' => 'form-control']) !!}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            {!! Form::submit("Save",['class' => 'btn settingBtn pull-right']) !!}
-                        </div>
-                        {!! Form::close() !!}
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="panel panelSettingData">
                 <div class="panel-heading" role="tab" id="headingSingle">
                     <h4 class="panel-title"><a role="button" data-toggle="collapse" data-parent="#accordion"
                                                href="#collapseSingle" aria-expanded="true"
@@ -128,39 +61,8 @@
                         {!! Form::hidden('id',null) !!}
                         <div class="form-group">
                             <div class="row">
-                                <div class="col-md-12">
-                                    {{Form::hidden('header',0)}}
-                                    {{Form::hidden('footer',0)}}
-                                    <label class="bd_layout pull-left m-r-15">{!! Form::checkbox('header',1,null,['style' => 'position:initial;z-index:1;']) !!}
-                                        <span class="labls">Header</span>
-
-                                    </label>
-                                    <label class="bd_layout"> {!! Form::checkbox('footer',1,null,['style' => 'position:initial;z-index:1;']) !!}
-                                        <span class="labls">Footer</span>
-
-                                    </label>
-                                </div>
-                                <div class="col-md-12 m-b-10">
-                                    <div class="col-sm-4 p-l-0">Page Layout</div>
-                                    <div class="col-sm-5 p-l-0 p-r-10">
-                                        <input data-key="title" readonly="readonly"
-                                               class="page-layout-title form-control"
-                                               value="@if($single->page_layout){!! BBgetLayoutAttr($single->page_layout,"title") !!}@else{!! 'Nothing selected' !!} @endif">
-                                    </div>
-                                    {!! BBbutton2('layouts','page_layout','front_pages_layout',($single->page_layout)?'Change':'Select',['class'=>'btn btn-default change-layout','model' =>$single->page_layout]) !!}
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="row">
                                 <div class="col-md-12 m-b-10">
                                     <div class="col-sm-4 p-l-0">Main Unit</div>
-                                    <div class="col-sm-5 p-l-0 p-r-10">
-                                        <input data-key="title" readonly="readonly"
-                                               class="page-layout-title form-control"
-                                               value="@if(isset($single->main_content)){!! BBgetUnitAttr($single->main_content,"title") !!}@else{!! 'Nothing selected' !!} @endif">
-                                    </div>
                                     {!! BBbutton2('unit','main_content','single_post',(isset($single->main_content) && $single->main_content)?'Change':'Select',['class'=>'btn btn-default change-layout','model' =>(isset($single->main_content) && $single->main_content) ?$single->main_content : null]) !!}
 
                                 </div>
