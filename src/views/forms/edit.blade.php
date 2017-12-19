f5a0db84cc32c32a2c14e3841bb1934647fcfe2d
terakyan <edo.terakyan@gmail.com>
2017-12-19 03:18:49 -0800

    edit

--- src/views/forms/edit.blade.php
+++ src/views/forms/edit.blade.php
@@ -1,13 +1,14 @@
 @extends( 'btybug::layouts.admin' )
 @section( 'content' )
     <div class="row">
-      Edit Form
+        <h2>Edit Form</h2>
         <div class="col-md-12">
             <div class="bty-panel-collapse 	bty-panel-cl-tomato">
                 <div>
                     <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#availableFields" aria-expanded="true">
                         <span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
                         <span class="title">Available Fields</span>
+                        <a class="bty-btn bty-btn-save bty-btn-cl-black bty-btn-size-sm pull-right m-r-10"><span>Save</span></a>
                     </a>
                 </div>
                 <div id="availableFields" class="collapse in" aria-expanded="true" style="">
@@ -25,13 +26,17 @@
                             @else
                                 No Columns Available
                             @endif
-
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
+            <h2>Preview Area</h2>
+
+            <div class="col-md-12 preview-area">
+
+            </div>
+        </div>
     </div>
 @stop
 @section( 'CSS' )
