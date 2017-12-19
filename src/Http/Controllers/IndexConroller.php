6f818faad2fbc394a224aca5f0fb51c331696614
Eduard <edo.terakyan@gmail.com>
2017-12-19 20:44:02 +0400

    fields

--- src/Http/Controllers/IndexConroller.php
+++ src/Http/Controllers/IndexConroller.php
@@ -117,7 +117,7 @@ class IndexConroller extends Controller
             }
 
             $html = \view("blog::_partials.render-fields",compact('data'))->render();
-            return \Response::json(['html' => $html,'error' => false,'fields' => array_merge($existing,($request->existings) ? json_decode($request->existings,true) : [])]);
+            return \Response::json(['html' => $html,'error' => false,'fields' => $data]);
         }
         return \Response::json(['message' => "Fields are invalid",'error' => true]);
     }
