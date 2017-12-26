059270b918997d336f5698f1550b077d0e8ca59e
sahakavatar <hakobyan.sahak88@gmail.com>
2017-12-26 23:07:08 +0400

    up

--- src/views/list.blade.php
+++ src/views/list.blade.php
@@ -1,9 +1,8 @@
 @extends('btybug::layouts.admin')
 @section('content')
 
-    <div role="tabpanel" class="m-t-10" id="main">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 main_container_11">
-            <table class="table table-bordered" id="users-table">
+            <table id="users-table" class="table table-striped table-bordered" cellspacing="0" width="100%">
                 <thead>
                 <tr>
                     <th>id</th>
@@ -16,18 +15,32 @@
                 </thead>
             </table>
         </div>
-    </div>
     @include('resources::assests.magicModal')
 @stop
 @section('CSS')
 @stop
 @section('JS')
     <script>
+
         $(function() {
             $('#users-table').DataTable({
                 processing: true,
                 serverSide: true,
                 ajax: '{!! route('postsData') !!}',
+                dom: 'Bfrtip',
+                columnDefs: [
+                    {
+                        targets: 1,
+                        className: 'noVis'
+                    }
+                ],
+                buttons: [
+                    {
+                        extend: 'colvis',
+                        columns: ':not(.noVis)'
+                    }
+                ]
+                ,
                 columns: [
                     { data: 'id', name: 'id' },
                     { data: 'description', name: 'description' },
