8b5bcd1910fbca40719be81392508c132a010a6f
sahakavatar <hakobyan.sahak88@gmail.com>
2017-12-28 11:54:50 +0400

    blog ui

--- src/Config/Gears/FrontGears/Units/all_post/tpl.blade.php
+++ src/Config/Gears/FrontGears/Units/all_post/tpl.blade.php
@@ -19,7 +19,8 @@
                 <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
                         @if(isset($settings["custom_sort"]))
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Sort<span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-expanded="false">Sort<span class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="#">sub 1</a></li>
                                 <li><a href="#">sub 2</a></li>
@@ -48,12 +49,92 @@
                 </ul>
                 @if(isset($settings["custom_search"]))
                     <form class="navbar-form text-center search-form" role="search">
-                        <input type="search" class="form-control" placeholder="Search" />
+                        <input type="search" class="form-control" placeholder="Search"/>
                     </form>
                 @endif
             </div>
         </div>
     </nav>
+    <div class="bty-all-blog">
+        <div class="container">
+            <div class="row">
+                <ul>
+                    <li class="col-md-4">
+                        <figure class="bty-recent-post-3">
+                            <img src="http://avante.biz/wp-content/uploads/Nice-Wallpapers/Nice-Wallpapers-006.jpg" alt="">
+                            <div>
+                                <span>27</span>
+                                <span>Sep</span>
+                            </div>
+                            <i class="fa fa-check-circle-o" aria-hidden="true"></i>
+                            <figcaption>
+                                <h3>Title lorem 1 lorem</h3>
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.
+                                </p>
+                                <button>Read More</button>
+                            </figcaption>
+                            <a href="#"></a>
+                        </figure>
+                    </li>
+                    <li class="col-md-4">
+                        <figure class="bty-recent-post-3">
+                            <img src="http://avante.biz/wp-content/uploads/Nice-Wallpapers/Nice-Wallpapers-006.jpg" alt="">
+                            <div>
+                                <span>27</span>
+                                <span>Sep</span>
+                            </div>
+                            <i class="fa fa-check-circle-o" aria-hidden="true"></i>
+                            <figcaption>
+                                <h3>Title lorem 1 lorem</h3>
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.
+                                </p>
+                                <button>Read More</button>
+                            </figcaption>
+                            <a href="#"></a>
+                        </figure>
+                    </li>
+                    <li class="col-md-4">
+                        <figure class="bty-recent-post-3">
+                            <img src="http://avante.biz/wp-content/uploads/Nice-Wallpapers/Nice-Wallpapers-006.jpg" alt="">
+                            <div>
+                                <span>27</span>
+                                <span>Sep</span>
+                            </div>
+                            <i class="fa fa-check-circle-o" aria-hidden="true"></i>
+                            <figcaption>
+                                <h3>Title lorem 1 lorem</h3>
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.
+                                </p>
+                                <button>Read More</button>
+                            </figcaption>
+                            <a href="#"></a>
+                        </figure>
+                    </li>
+                    <li class="col-md-4">
+                        <figure class="bty-recent-post-3">
+                            <img src="http://avante.biz/wp-content/uploads/Nice-Wallpapers/Nice-Wallpapers-006.jpg" alt="">
+                            <div>
+                                <span>27</span>
+                                <span>Sep</span>
+                            </div>
+                            <i class="fa fa-check-circle-o" aria-hidden="true"></i>
+                            <figcaption>
+                                <h3>Title lorem 1 lorem</h3>
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.
+                                </p>
+                                <button>Read More</button>
+                            </figcaption>
+                            <a href="#"></a>
+                        </figure>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 
     <div class="container">
@@ -65,8 +146,8 @@
                             <div class="col-lg-4 col-md-4">
                                 <aside>
                                     @if(isset($post->image))
-                                    <img src="{!! url($post->image) !!}"
-                                         class="img-responsive">
+                                        <img src="{!! url($post->image) !!}"
+                                             class="img-responsive">
                                     @else
                                         <img src="https://s-media-cache-ak0.pinimg.com/originals/2a/f7/2f/2af72f34c04fbf5354c4d637e98969a8.jpg"
                                              class="img-responsive">
@@ -77,11 +158,15 @@
                                         </div>
                                     </div>
                                     <div class="content-footer">
-                                        <img class="user-small-img" src="{!! url(BBGetUserAvatar($post->author_id)) !!}">
-                                        <span style="font-size: 16px;color: #fff;"><a href="">{!! BBGetUserName($post->id) !!}</a></span>
+                                        <img class="user-small-img"
+                                             src="{!! url(BBGetUserAvatar($post->author_id)) !!}">
+                                        <span style="font-size: 16px;color: #fff;"><a
+                                                    href="">{!! BBGetUserName($post->id) !!}</a></span>
                                         <span class="pull-right">
-                                            <a href="#" data-toggle="tooltip" data-placement="left" title="Comments"><i class="fa fa-comments"></i> 30</a>
-                                            <a href="#" data-toggle="tooltip" data-placement="right" title="Loved"><i class="fa fa-heart"></i> 20</a>
+                                            <a href="#" data-toggle="tooltip" data-placement="left" title="Comments"><i
+                                                        class="fa fa-comments"></i> 30</a>
+                                            <a href="#" data-toggle="tooltip" data-placement="right" title="Loved"><i
+                                                        class="fa fa-heart"></i> 20</a>
                                         </span>
                                     </div>
                                 </aside>
